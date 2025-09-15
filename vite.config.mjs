beda40444b1efec8626289c53a93335c85179ea9
nitinkoberoii <lonewolfindia15@gmail.com>
2025-09-15 23:46:21 +0530

    change config

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: "build",
+    outDir: "dist",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
@@ -16,6 +16,6 @@ export default defineConfig({
     port: "4028",
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  }
+    allowedHosts: [".amazonaws.com", ".builtwithrocket.new"],
+  },
 });
